355d8d44312298149bb131ea2b4789045c364a84
Mike Mannix <mike.mannix@gmail.com>
2021-01-29 10:36:10 -0500

    First real basic home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="flex flex-col inline-flex space-y-6 font-mono">
+      <div>
+        <div className="text-3xl">
+          &nbsp;&nbsp;&nbsp;mik<div className="text-gray-400 inline">e</div>
+        </div>
+        <div className="text-3xl">
+          <div className="text-gray-400 inline">man</div>nix
+        </div>
+      </div>
+      <div className="text-lg">
+        {/* eslint-disable jsx-a11y/accessible-emoji */}
+        Hi &#128075;
+      </div>
+      <div className="flex flex-row  p-2 justify-items-center underline bg-gray-100 border rounded-lg border-gray-200 text-sm space-x-4">
+        <div className="text-center">
+          <a href="https://twitter.com/miknix/">Twitter</a>
+        </div>
+        <div className="text-center">
+          <a href="https://github.com/mannix/">GitHub</a>
+        </div>
+      </div>
+    </div>
+  )
 }
